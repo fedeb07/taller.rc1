6585cbc8e2350847fe96939ebae0c9e5eaa8c00b
Federica Barrera <fede@MacBook-Air-de-Feder.local>
2026-05-19 11:25:31 -0500

    ingreso de nnumero de estudiantes

---
+++ actividad.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int estudiantes;
+
+    printf("Ingrese el numero de estudiantes: ");
+    scanf("%d", &estudiantes);
+
+    float notas[estudiantes][3];
+
+    return 0;
+}
